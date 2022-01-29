aed2574f4cee53ea5caa0699d192ed9a1e8b6e10
Michael <kmiecik013@gmail.com>
2022-01-28 17:49:11 -0700

    finished the final exerercises in Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,78 +1,125 @@
 import React, { Component } from "react";
-import Loading from "./Loading";
-import Panel from "./Panel"
 
 import classnames from "classnames";
-
+import Loading from "./Loading";
+import Panel from "./Panel";
+import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from "helpers/selectors";
+import { setInterview } from "helpers/reducers";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
-  }
+    getValue: getInterviewsPerDay,
+  },
 ];
 
 class Dashboard extends Component {
-
   state = {
-    loading: false,
+    loading: true,
     focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {},
+  };
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+      });
+    });
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState((previousState) =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
   }
 
   selectPanel(id) {
-    this.setState(previousState => ({
-      focused: previousState.focused !== null ? null : id
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
     }));
   }
 
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
 
-render() {
-  const dashboardClasses = classnames("dashboard", {
-    "dashboard--focused": this.state.focused
-  });
+    if (this.state.loading) {
+      console.log("state check:", this.state);
+      return <Loading />;
+    }
 
+    // Map over the data array and create a new Panel for each of the four data objects. Render the panels array as children of the main element.
+    const panels = data
+      // Use the this.state.focused value to filter panel data before converting it to components.
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => (
+        <Panel
+          key={panel.id}
+          label={panel.label}
+          value={panel.getValue(this.state)}
+          onSelect={() => this.selectPanel(panel.id)}
+        />
+      ));
 
-  if (this.state.loading) {
-    return <Loading />;
+    return <main className={dashboardClasses}>{panels}</main>;
   }
-
-  const panels = data
-
-    .filter(
-      panel => this.state.focused === null || this.state.focused === panel.id
-    )
-    .map(panel => (
-
-      <Panel
-        key={panel.id}
-        label={panel.label}
-        value={panel.value}
-        onSelect={event => this.selectPanel(panel.id)}
-      />
-    ));
-
-  return <main className={dashboardClasses}>{panels}</main>;
 }
-}
-
-    
-  
-
 
 export default Dashboard;
